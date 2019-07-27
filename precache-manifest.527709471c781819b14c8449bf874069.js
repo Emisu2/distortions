beb41f77ede8b85834e2d9fb040cbf09929bbf13
Emily Roberts <e.baker174@gmail.com>
2019-07-27 17:25:42 -0400

    Updates

---
+++ precache-manifest.527709471c781819b14c8449bf874069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdc98f8e7099cb9ba6c29da54585157",
+    "url": "/playtime/index.html"
+  },
+  {
+    "revision": "f50377e84e37753c2afa",
+    "url": "/playtime/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3a0a27c0925233e473a6",
+    "url": "/playtime/static/css/main.2f38fb8f.chunk.css"
+  },
+  {
+    "revision": "f50377e84e37753c2afa",
+    "url": "/playtime/static/js/2.a39019fa.chunk.js"
+  },
+  {
+    "revision": "3a0a27c0925233e473a6",
+    "url": "/playtime/static/js/main.4081418b.chunk.js"
+  },
+  {
+    "revision": "2a5918ad4fa8104bdb41",
+    "url": "/playtime/static/js/runtime~main.cb9a5612.js"
+  },
+  {
+    "revision": "c04b8f5567a2dd47cb052d66bae612e1",
+    "url": "/playtime/static/media/brankovic.c04b8f55.ttf"
+  }
+]);
